1ab48e7e84b757b3e1ce53d2d1da4fce1a31c580
erfan-rd226 <erfan.rajabzade0226@gmail.com>
2023-10-01 12:21:21 +0330

    send data for client

--- send_location/serializers.py
+++ send_location/serializers.py
@@ -1,8 +1,34 @@
-from .models import Location
+from .models import Location , Device
 from rest_framework import serializers
 
 
 class LocationSerializers(serializers.ModelSerializer):
     class Meta:
         model = Location
-        fields = ["device_id", "lat", "lon", "speed"]
+        fields = ["id", "device_id", "lat", "lon", "speed", "date_time"]
+    
+    def create(self, validated_data):
+        location = Location.objects.create(
+            device_id=validated_data["device_id"],
+            lat = validated_data["lat"],
+            lon = validated_data["lon"],
+            speed = validated_data["speed"]
+        )
+        device = Device.objects.get(unique_id=int(validated_data["device_id"]))
+        # laa = Device.objects.get(unique_id=float(validated_data["lat"]))
+        # lonn = Device.objects.get(unique_id=float(validated_data["lon"]))
+    
+        device.last_position = location
+        # laa.last_position = location
+        # lonn.last_position = location
+        
+        device.save()
+        # laa.save()
+        # lonn.save()
+
+        return location
+
+class DeviceSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Device
+        fields = ['last_position']
--- send_location/views.py
+++ send_location/views.py
@@ -1,29 +1,30 @@
 import datetime
 from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import HttpResponse
+from send_location.models import Device, Location
 from django.views.decorators.csrf import csrf_exempt
-from .models import Location
-from .serializers import LocationSerializers
+from .serializers import LocationSerializers, DeviceSerializers
 
 class LocationApi(APIView):
 
-    def get(self, request):
-        device_id = request.GET.get("id")
-        lat = request.GET.get("lat")
-        lon = request.GET.get("lon")
-        speed = request.GET.get("speed")
+    def post(self, request):
+        serializer = LocationSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            print(serializer.errors)
+            return Response(serializer.errors, status=400)
 
-        data = {
-            "device_id": device_id,
-            "lat": lat,
-            "lon": lon,
-            "speed": speed,
-            "date_time": datetime.datetime.now()
-        }
-        serializer =  LocationSerializers(data=data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+        
 
-        return Response(serializer.data, status=201)
+
+# class DeviceApi(APIView):
+
+#     def get(self,request):
+#         device = Device.objects.all()
+#         serializer = DeviceSerializers(device)
+#         serializer.save()
